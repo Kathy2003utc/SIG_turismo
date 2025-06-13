ae49f2eec34cb93a73d1661f91681be61d3bad34
Katherin <katherin.pico2124@utc.edu.ec>
2025-06-12 22:12:28 -0500

    Mejor diseño de la plantilla

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Turismos FK</title>
 	<meta charset="utf-8">
+	<title>Turismos FK</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- jQuery -->
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
 	
-    <!-- jQuery -->
-	<script src="https://code.jquery.com/jquery-3.7.1.js" crossorigin="anonymous"></script>
+	<!-- jQuery Validation Plugin -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
 
-	<!-- jQuery Validation -->
-	<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
-
-	<!-- Bootstrap CSS & JS -->
+	<!-- Bootstrap 5 CSS & JS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous" />
+
+	<!-- Bootstrap Icons -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
 	<!-- SweetAlert2 -->
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -26,166 +29,154 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.5.4/js/fileinput.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.5.0/js/locales/es.min.js"></script>
 
-	<!-- DataTables v1.13.7 + Bootstrap 5 -->
+	<!-- DataTables -->
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+	<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+
 	<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 	<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
-
-	<!-- DataTables Buttons -->
-	<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
 	<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
 	<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
 	<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
-
-	<!-- Exportación (ZIP, PDF) -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 
-	<!--Idioma español para agregar la imagen-->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.5.0/js/locales/es.min.js"></script>
-
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-
-
-	<!-- Icono del navegador -->
-	<link rel="icon" href="{% static 'plantilla/kaiadmin/assets/img/kaiadmin/favicon.ico' %}" type="image/x-icon" />
-
-
-    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdYzugNC_QlesLopg6J4884TRsBzvusjg&callback=initMap"></script>
-
-
+	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Arizonia&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Estilos personalizados -->
+	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-	<link rel="stylesheet" href="{{ asset('css/animate.css')}}">
-	<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css')}}">
-	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}">
-	<link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
-	<link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css')}}">
-	<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css')}}">
-	<link rel="stylesheet" href="{{ asset('css/flaticon.css')}}">
-	<link rel="stylesheet" href="{{ asset('css/style.css')}}">
+	<!-- Icono -->
+	<link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-		<div class="container">
-			<a class="navbar-brand" href="index.html">Pacific<span>Agencia CRUD</span></a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="oi oi-menu"></span> Menu
-			</button>
 
+	<!-- NAVBAR -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="ftco-navbar">
+		<div class="container">
+			<a class="navbar-brand" href="{{ url('/') }}">Pacific<span> Agencia CRUD</span></a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 			<div class="collapse navbar-collapse" id="ftco-nav">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-					<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-					<li class="nav-item"><a href="destination.html" class="nav-link">Destination</a></li>
-					<li class="nav-item"><a href="hotel.html" class="nav-link">Hotel</a></li>
-					<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-					<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item active"><a href="{{ url('/') }}" class="nav-link">Home</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">About</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Destination</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Hotel</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
 				</ul>
 			</div>
 		</div>
 	</nav>
 
-    @yield('contenido')
+	<!-- CONTENIDO -->
+	@yield('contenido')
 
-    <footer class="ftco-footer bg-bottom ftco-no-pt" style="background-image: url(images/bg_3.jpg);">
+	<!-- FOOTER -->
+	<footer class="ftco-footer bg-bottom" style="background-image: url('{{ asset('images/bg_3.jpg') }}');">
 		<div class="container">
 			<div class="row mb-5">
+				<!-- Columna 1 -->
 				<div class="col-md pt-5">
 					<div class="ftco-footer-widget pt-md-5 mb-4">
 						<h2 class="ftco-heading-2">About</h2>
-						<p>Somos unos estudiantes que empezaron un emprendimiento de Turismo alrededor del Ecuador</p>
-						<ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+						<p>Somos estudiantes emprendedores del turismo en Ecuador.</p>
+						<ul class="ftco-footer-social list-unstyled">
 							<li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
 							<li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
 							<li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
 						</ul>
 					</div>
 				</div>
+
+				<!-- Columna 2 -->
 				<div class="col-md pt-5 border-left">
 					<div class="ftco-footer-widget pt-md-5 mb-4 ml-md-5">
-						<h2 class="ftco-heading-2">Infromation</h2>
+						<h2 class="ftco-heading-2">Información</h2>
 						<ul class="list-unstyled">
 							<li><a href="#" class="py-2 d-block">Pico Katherin</a></li>
 							<li><a href="#" class="py-2 d-block">Trujillo Israel</a></li>
-							<li><a href="#" class="py-2 d-block">Agente Turistico</a></li>
+							<li><a href="#" class="py-2 d-block">Agente Turístico</a></li>
 							<li><a href="#" class="py-2 d-block">Privacidad</a></li>
-							<li><a href="#" class="py-2 d-block">Politicas empresariales</a></li>
-							<li><a href="#" class="py-2 d-block">Llamanos a: 0990960365, 0958615103</a></li>
+							<li><a href="#" class="py-2 d-block">Políticas empresariales</a></li>
+							<li><a href="#" class="py-2 d-block">Llámanos: 0990960365, 0958615103</a></li>
 						</ul>
 					</div>
 				</div>
+
+				<!-- Columna 3 -->
 				<div class="col-md pt-5 border-left">
 					<div class="ftco-footer-widget pt-md-5 mb-4">
-						<h2 class="ftco-heading-2">Experience</h2>
+						<h2 class="ftco-heading-2">Experiencias</h2>
 						<ul class="list-unstyled">
 							<li><a href="#" class="py-2 d-block">Aventura</a></li>
 							<li><a href="#" class="py-2 d-block">Hotel y Restaurante</a></li>
 							<li><a href="#" class="py-2 d-block">Playa de Manta</a></li>
 							<li><a href="#" class="py-2 d-block">Viaje al Oriente</a></li>
-							<li><a href="#" class="py-2 d-block">Campamento en el boliche</a></li>
-							<li><a href="#" class="py-2 d-block">Fiesta en la cobacha</a></li>
+							<li><a href="#" class="py-2 d-block">Campamento en el Boliche</a></li>
+							<li><a href="#" class="py-2 d-block">Fiesta en la Cobacha</a></li>
 						</ul>
 					</div>
 				</div>
+
+				<!-- Columna 4 -->
 				<div class="col-md pt-5 border-left">
 					<div class="ftco-footer-widget pt-md-5 mb-4">
-						<h2 class="ftco-heading-2">Have a Questions?</h2>
-						<div class="block-23 mb-3">
-							<ul>
-								<li><span class="icon fa fa-map-marker"></span><span class="text">Universidad Tecnica de Cotopaxi (UTC), San Felipe</span></li>
-								<li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+593 981864430</span></a></li>
-								<li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">fabian@yourdomain.com</span></a></li>
-							</ul>
-							</div>
+						<h2 class="ftco-heading-2">¿Preguntas?</h2>
+						<ul class="list-unstyled">
+							<li><span class="icon fa fa-map-marker"></span> Universidad Técnica de Cotopaxi, San Felipe</li>
+							<li><a href="#"><span class="icon fa fa-phone"></span> +593 981864430</a></li>
+							<li><a href="#"><span class="icon fa fa-envelope"></span> fabian@yourdomain.com</a></li>
+						</ul>
 					</div>
 				</div>
 			</div>
+
+			<!-- Copyright -->
 			<div class="row">
 				<div class="col-md-12 text-center">
-
-					<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-					</div>
+					<p>
+						Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados |
+						Hecho con <i class="fa fa-heart"></i> por <a href="https://colorlib.com" target="_blank">Colorlib</a>
+					</p>
 				</div>
 			</div>
-		</footer>
-			
-			
+		</div>
+	</footer>
 
-		<!-- loader -->
-		<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+	<!-- Loader -->
+	<div id="ftco-loader" class="show fullscreen">
+		<svg class="circular" width="48px" height="48px">
+			<circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
+			<circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#F96D00"/>
+		</svg>
+	</div>
 
+	<!-- Scripts personalizados -->
+	<script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
+	<script src="{{ asset('js/popper.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+	<script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.animateNumber.min.js') }}"></script>
+	<script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
+	<script src="{{ asset('js/scrollax.min.js') }}"></script>
+	<script src="{{ asset('js/google-map.js') }}"></script>
+	<script src="{{ asset('js/main.js') }}"></script>
 
-		<script src="{{ asset('js/jquery.min.js')}}"></script>
-		<script src="{{ asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
-		<script src="{{ asset('js/popper.min.js')}}"></script>
-		<script src="{{ asset('js/bootstrap.min.js')}}"></script>
-		<script src="{{ asset('js/jquery.easing.1.3.js')}}"></script>
-		<script src="{{ asset('js/jquery.waypoints.min.js')}}"></script>
-		<script src="{{ asset('js/jquery.stellar.min.js')}}"></script>
-		<script src="{{ asset('js/owl.carousel.min.js')}}"></script>
-		<script src="{{ asset('js/jquery.magnific-popup.min.js')}}"></script>
-		<script src="{{ asset('js/jquery.animateNumber.min.js')}}"></script>
-		<script src="{{ asset('js/bootstrap-datepicker.js')}}"></script>
-		<script src="{{ asset('js/scrollax.min.js')}}"></script>
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-		<script src="{{ asset('js/google-map.js')}}"></script>
-		<script src="{{ asset('js/main.js')}}"></script>
-			
 </body>
-<script src="//cdn.datatables.net/2.3.1/js/dataTables.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.5.4/js/fileinput.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.5.0/js/locales/es.min.js"></script>
 </html>
