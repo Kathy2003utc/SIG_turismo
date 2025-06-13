2712cd4de9c585ee97310ce88fae00bb6189066a
Katherin <katherin.pico2124@utc.edu.ec>
2025-06-12 20:08:32 -0500

    Modificacion del mapa global

--- resources/views/turismos/mapa.blade.php
+++ resources/views/turismos/mapa.blade.php
@@ -9,29 +9,57 @@
 </div>
 
 <script type="text/javascript">
-    function initMap(){
-        //alert("mapa ok");
-        var latitud_longitud= new google.maps.LatLng(-0.9374805,-78.6161327);
-        var mapa=new google.maps.Map(
+    function initMap() {
+        var latitud_longitud = new google.maps.LatLng(-0.9374805, -78.6161327);
+
+        var mapa = new google.maps.Map(
             document.getElementById('mapa-turismo'),
             {
-                center:latitud_longitud,
-                zoom:15,
-                mapTypeId:google.maps.MapTypeId.ROADMAP
+                center: latitud_longitud,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
             }
         );
-        @foreach($turismos as $turismos)
-            var cordenadaCliente= new google.maps.LatLng({{$turismos->latitud}}, 
-                                        {{$turismos->longitud}});
-            var marcador=new google.maps.Marker({
-                position:cordenadaCliente,
-                map:mapa,
-                icon:"https://cdn-icons-png.flaticon.com/512/4899/4899329.png",
-                draggable:false
+
+        // Recorremos los lugares turísticos desde el backend
+        @foreach($turismos as $turismo)
+            var coordenada = new google.maps.LatLng({{ $turismo->latitud }}, {{ $turismo->longitud }});
+
+            var marcador = new google.maps.Marker({
+                position: coordenada,
+                map: mapa,
+                icon: {
+                url: "https://cdn-icons-png.flaticon.com/512/4899/4899329.png",
+                scaledSize: new google.maps.Size(50, 50)
+                },
+                draggable: false
+            });
+
+            // Contenido del cuadro de información
+            var contenido = `
+                <div style="font-family:sans-serif;">
+                    <h5>{{ $turismo->nombre }}</h5>
+                    <p><strong>Categoría:</strong> {{ $turismo->categoria }}</p>
+                    <p>{{ $turismo->descripcion }}</p>
+                    @if($turismo->imagenes)
+                        <img src="{{ Storage::url($turismo->imagenes) }}" alt="Imagen" width="150">
+                    @endif
+                </div>
+            `;
+
+            // Instancia de InfoWindow
+            var infoWindow = new google.maps.InfoWindow({
+                content: contenido
+            });
+
+            // Mostrar InfoWindow al hacer clic
+            marcador.addListener('click', function() {
+                infoWindow.open(mapa, marcador);
             });
         @endforeach
     }
 </script>
+
 
 
 @endsection
